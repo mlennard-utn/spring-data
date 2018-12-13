cbeb8811c5e09adc299af5468e2c5a2524a30ff4
Martin Lennard <mlennard.utn@gmail.com>
2018-12-12 21:52:29 -0300

    Modificaciones en el proyecto para utilizar BBDD MySQL

--- src/main/java/hello/Person.java
+++ src/main/java/hello/Person.java
@@ -1,18 +1,25 @@
 package hello;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="person")
 public class Person {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private long id;
 
+	@Column(name="first_name")
 	private String firstName;
+	
+	@Column(name="last_name")
 	private String lastName;
 
 	public String getFirstName() {
