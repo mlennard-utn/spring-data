330d25e6c0a8936124a98579e97e311ee6c86302
alex de assis <alexdeassis@gmail.com>
2018-12-13 22:10:19 -0300

    Post mapping and form

--- src/main/java/hello/PersonController.java
+++ src/main/java/hello/PersonController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class PersonController {
@@ -17,7 +19,14 @@ public class PersonController {
 		Iterable<Person> personas = personRepository.findAll();
 		
 		model.addAttribute("personas", personas);
-		
+		Person p = new Person();
+		model.addAttribute("person", p);
+		return "persons";
+	}
+	
+	@PostMapping(value="/personas", consumes="application/x-www-form-urlencoded;charset=UTF-8")
+	public String savePerson(Person persona) {
+		Person p = personRepository.save(persona);
 		return "persons";
 	}
 	
